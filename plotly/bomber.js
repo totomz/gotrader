6542872d38ad58ac947fb337ebc0a01d6652ae26
tommaso.doninelli <tommaso.doninelli@my-ideas.it>
2021-09-09 06:12:19 +0200

    synced zoom in charts

--- plotly/bomber.js
+++ plotly/bomber.js
@@ -1,25 +1,86 @@
-function Init(){
-    const trace1 = {
-      type: 'bar',
-      x: [1, 2, 3, 4],
-      y: [5, 10, 2, 8],
-      marker: {
-          color: '#C8A2C8',
-          line: {
-              width: 2.5
-          }
-      }
-    };
-    
-    const data = [ trace1 ];
-    
-    const layout = { 
-      title: 'Responsive to window\'s size!',
-      font: {size: 18}
-    };
-    
-    const config = {responsive: true}
-    
-    Plotly.newPlot('myDiv', data, layout, config );
-
+function uniqId(name) {
+    return `${name}_${Math.round(new Date().getTime() + (Math.random() * 100))}`;
 }
+
+async function addCharts() {
+
+    const dataset = await $.get(DATASET_URL);
+    dataset_golobal = dataset;
+    console.log("downloaded dataset", dataset);
+    
+    Object.keys(dataset).forEach(k => {
+        $("#serieList").append(new Option(k, k));
+    });
+
+
+    // Put your signals here
+    const charts = [{
+        trace: [{
+            name: 'Volume',
+            type: 'bar',
+            x: dataset_golobal.candles.x,
+            y: dataset_golobal.candles.volume,
+        }],
+        layout,
+        options
+    }, {
+        trace: [{
+            name: 'Cash',
+            type: 'scatter',
+            x: dataset_golobal.cash.x,
+            y: dataset_golobal.cash.y,
+        }],
+        layout,
+        options
+    }, {
+        trace: [{
+            masterZoom: true,
+            name: 'Candles',
+            type: 'candlestick',
+
+            x: dataset.candles.x,
+            close: dataset.candles.close,
+            high: dataset.candles.high,
+            low: dataset.candles.low,
+            open: dataset.candles.open,
+
+            decreasing: {line: {color: '#7F7F7F'}},
+            increasing: {line: {color: '#17BECF'}},
+            line: {color: 'rgba(31,119,180,1)'},
+
+            xaxis: 'x',
+            yaxis: 'y'
+        }],
+        layout: {...layout, ...{height: 600}},
+        options: {displayModeBar: true}
+    }];
+
+    charts.forEach(c => {
+        const divId = uniqId(`chart_${c.trace[0].name}`)
+        $("#chartPanel").append(`<div id="${divId}">`);
+        Plotly.newPlot(divId, c.trace, c.layout, c.options);
+
+        if (divId.startsWith('chart_Candles_')) {
+            const div = document.getElementById(divId);
+            div.on("plotly_relayout", function (ed) {
+                relayout(ed);
+            });
+        } else {
+            $(`#${divId}`).addClass("chartSlave")
+        }
+    });
+}
+
+function relayout(ed) {
+    const divs = document.getElementsByClassName('chartSlave');
+    for (let i = 0; i< divs.length; i++) {
+        const div = divs[i]
+        var update = {
+            'xaxis.range[0]': ed["xaxis.range[0]"],
+            'xaxis.range[1]': ed["xaxis.range[1]"],
+            'xaxis.autorange': ed["xaxis.autorange"],
+        };
+        Plotly.relayout(div, update);
+    }
+}
+
